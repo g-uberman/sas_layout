23b9317442388e802432f684138738c64b8da8b9
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-02 16:38:48 +0100

    navbar search

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,8 +3,8 @@ import { Search } from '@mui/icons-material';
 export const NavBar = () => {
   return (
     <div id="navbar">
-      <div className="cell"></div>
-      <div className="cell">
+      <div className="cell left"></div>
+      <div className="cell center">
         <button>About SAS</button>
         <button>Sustainability</button>
         <button>Investor relations</button>
@@ -12,7 +12,7 @@ export const NavBar = () => {
         <button>Career</button>
         <button>Contact</button>
       </div>
-      <div className="cell"><Search color="primary"/></div>
+      <div className="cell right"><Search id="search" /></div>
     </div>
   );
 };
