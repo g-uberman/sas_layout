7e7e14c8e035550e867a9b2f55bb97ac9d3f1bc1
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-02 18:30:59 +0100

    carousel static

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,3 +1,18 @@
+import { Button } from "@mui/material";
+
 export const Carousel = () => {
-    return <></>
-}
+  return (
+    <div id="carousel" className="first">
+      <div className="blueBox">
+        <div className="white">Interim report Q4</div>
+        <div className="gold">2022</div>
+      </div>
+      <Button variant="contained">Read the report</Button>
+      <section>
+        <button className="smallSquare"> </button>
+        <button className="smallSquare"> </button>
+        <button className="smallSquare"> </button>
+      </section>
+    </div>
+  );
+};
