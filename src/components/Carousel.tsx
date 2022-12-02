10c6cdab88316844deca5e9405cafac82ca2cc90
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-02 19:08:16 +0100

    carousel navigation

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,83 @@
+import { useState } from "react";
 import { Button } from "@mui/material";
 
 export const Carousel = () => {
+  const [carousel, setCarousel] = useState(0);
+
   return (
-    <div id="carousel" className="first">
-      <div className="blueBox">
-        <div className="white">Interim report Q4</div>
-        <div className="gold">2022</div>
-      </div>
-      <Button variant="contained">Read the report</Button>
-      <section>
-        <button className="smallSquare"> </button>
-        <button className="smallSquare"> </button>
-        <button className="smallSquare"> </button>
-      </section>
-    </div>
+    <>
+      {carousel === 0 && (
+        <div id="carousel" className="zero">
+          <div className="blueBox">
+            <div className="white">
+              SAS' business <br /> transformation plan{" "}
+            </div>
+          </div>
+          <Button variant="contained">Read about the chapter 11 process</Button>
+          <div id="carouselControl">
+            <button
+              className="blueSquare active"
+              onClick={() => setCarousel(0)}
+            ></button>
+            <button
+              className="blueSquare"
+              onClick={() => setCarousel(1)}
+            ></button>
+            <button
+              className="blueSquare"
+              onClick={() => setCarousel(2)}
+            ></button>
+          </div>
+        </div>
+      )}
+
+      {carousel === 1 && (
+        <div id="carousel" className="one">
+          <div className="blueBox">
+            <div className="white">Interim report Q4</div>
+            <div className="gold">2022</div>
+          </div>
+          <Button variant="contained">Read the report</Button>
+          <div id="carouselControl">
+            <button
+              className="blueSquare"
+              onClick={() => setCarousel(0)}
+            ></button>
+            <button
+              className="blueSquare active"
+              onClick={() => setCarousel(1)}
+            ></button>
+            <button
+              className="blueSquare"
+              onClick={() => setCarousel(2)}
+            ></button>
+          </div>
+        </div>
+      )}
+
+      {carousel === 2 && (
+        <div id="carousel" className="two">
+          <div className="blueBox">
+            <div className="white">Sustainability</div>
+            <div className="gold">at SAS</div>
+          </div>
+          <Button variant="contained">Read more</Button>
+          <div id="carouselControl">
+            <button
+              className="blueSquare"
+              onClick={() => setCarousel(0)}
+            ></button>
+            <button
+              className="blueSquare"
+              onClick={() => setCarousel(1)}
+            ></button>
+            <button
+              className="blueSquare active"
+              onClick={() => setCarousel(2)}
+            ></button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
