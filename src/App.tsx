b695516f738a5e9415c1d1653bcf8c9ae78c60e1
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-02 16:23:17 +0100

    navbar start

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from './components/Header';
+import { NavBar } from './components/NavBar';
 
 function App() {
   return (
     <>
     <Header />
+    <NavBar />
     </>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,18 @@
+import { Search } from '@mui/icons-material';
+
 export const NavBar = () => {
-    return <></>
-}
+  return (
+    <div id="navbar">
+      <div className="cell"></div>
+      <div className="cell">
+        <button>About SAS</button>
+        <button>Sustainability</button>
+        <button>Investor relations</button>
+        <button>Newsroom</button>
+        <button>Career</button>
+        <button>Contact</button>
+      </div>
+      <div className="cell"><Search color="primary"/></div>
+    </div>
+  );
+};
