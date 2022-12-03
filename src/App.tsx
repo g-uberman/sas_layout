6382fe644e26b22d406f97fb64c8116294fe9135
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-03 18:32:40 +0100

    header sticky

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Cards } from './components/Cards';
 import { Carousel } from './components/Carousel';
 import { Header } from './components/Header';
-import { NavBar } from './components/NavBar';
+// import { NavBar } from './components/NavBar';
 import { Supplier } from './components/Supplier';
 
 function App() {
   return (
     <>
     <Header />
-    <NavBar />
+    {/* <NavBar /> */}
     <Carousel />
     <Cards />
     <Supplier />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
 import logo from "../images/logo.svg";
+import { NavBar } from "./NavBar";
 
 export const Header = () => {
   return (
     <div id="header">
-      <img src={logo} />
+      <div id="logoBar">
+        <img src={logo} />
+      </div>
+      <NavBar />
     </div>
   );
 };
