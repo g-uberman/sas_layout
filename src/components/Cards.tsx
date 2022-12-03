144b2d3527cae9e067fa0e55f0eb53e9f18bfd29
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-03 13:53:36 +0100

    minor fixes

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -4,23 +4,20 @@ export const Cards = () => {
   return (
     <div id="cardWrapper">
       <div className="card">
-        <h2>Latest News</h2>
-        card1
-        <Button variant="contained" size="large" disableElevation>
+        <h3>Latest News</h3>
+        <Button variant="contained" size="large">
           See all events
         </Button>
       </div>
       <div className="card">
-        <h2>SAS Events</h2>
-        card2
-        <Button variant="contained" size="large" disableElevation>
+        <h3 className="blue">SAS Events</h3>
+        <Button variant="contained" size="large">
           See all events
         </Button>
       </div>
       <div className="card">
-        <h2>Interim Report Q4 2022</h2>
-        card3
-        <Button variant="contained" size="large" disableElevation>
+        <h3>Interim Report Q4 2022</h3>
+        <Button variant="contained" size="large">
           Read the report
         </Button>
       </div>
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './styles/style.css';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './styles/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
