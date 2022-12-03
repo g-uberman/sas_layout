01a3d423f3532ccef14ea0a7d38284eacbdcb16e
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-03 19:02:47 +0100

    header shrinking

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import logo from "../images/logo.svg";
 import { NavBar } from "./NavBar";
 
 export const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
+  window.onscroll = () => {scrollHandler()};
+
+  function scrollHandler() {
+    if (document.body.scrollTop > 152 || document.documentElement.scrollTop > 152) {
+      setScrolled(true);
+    } else {
+      if (scrolled) setScrolled(false);
+    }
+  }
+
   return (
     <div id="header">
+      {!scrolled && (
       <div id="logoBar">
         <img src={logo} />
       </div>
+      )}
+      {scrolled && (
+      <div id="logoBar" className="scrolled">
+        <img src={logo} />
+      </div>
+      )}
       <NavBar />
     </div>
   );
