4e668b0b07dc1effbdd85b1a89befd0adf223068
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-05 21:11:35 +0100

    instagram posts

--- src/components/SocialMedia/Instagram.tsx
+++ src/components/SocialMedia/Instagram.tsx
@@ -1,7 +1,44 @@
 export const Instagram = () => {
-    return (
-    <>
-    Instagram
-    </>
-    )
-}
+  return (
+    <div className="mediaWrapper">
+      <article>
+        <div className="image img1"></div>
+        <p>
+        Travelers for 75 years 🇩🇰🇳🇴🇸🇪 August 1st marks our 75th anniversary as
+        the proud carrier of travelers to, from and within Scandinavia. And
+        while a lot has happened since our first-ever flight from Stockholm to
+        New York 75 years ago, we’re even more excited about what has yet to
+        come. Thank you, for traveling with us for all these years! We are
+        looking forward to welcoming you on board our flights today, tomorrow
+        and for many more years to come. #flysas #SAS75 #wearetravelers
+        </p>
+      </article>
+      <article>
+        <div className="image img2"></div>
+        <p>
+        Travelers explore Pula 🇭🇷 Rising above the Adriatic Sea on the southern
+        tip of the peninsula is Pula. Besides being home to one of the world's
+        best preserved Roman amphitheaters, Pula is also known for its unbroken
+        blue coastal line, amazing beaches, great restaurants and its relaxed
+        Mediterranean atmosphere. Fly direct to Pula from Copenhagen, Stockholm
+        and Oslo this summer. We’ll get you there safely. #flysas
+        #wearetravelers #croatia #pula
+        </p>
+      </article>
+      <article>
+        <div className="image img3"></div>
+        <p>
+        Scandinavia is rooting for you! 🇩🇰🇳🇴🇸🇪 We are thrilled to be proud
+        carrier of the Scandinavian Olympic athletes! Today, the second flight
+        bound for the Summer Games left Oslo. With a quick pit-stop in
+        Copenhagen, to pick up fellow Scandinavian athletes, the flight is now
+        safely on its way to Fukuoka in Japan. The athletes will be traveling on
+        one of our brand-new A350’s, which is one of the most fuel-efficient
+        aircraft in the industry and our fleet’s latest pride and joy. Join us
+        in wishing the athletes all the best of luck at this year’s games!
+        #flysas #wearetravelers #japan #tokyo
+        </p>
+      </article>
+    </div>
+  );
+};
