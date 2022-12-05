b77ccc9d1701fb877d9a07be653f895891899300
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-05 23:18:33 +0100

    social media links

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,3 +1,50 @@
+import {
+  Facebook,
+  Twitter,
+  Instagram,
+  LinkedIn,
+  YouTube,
+} from "@mui/icons-material/";
+
 export const Links = () => {
-    return <>Links</>
-}
+  return (
+    <div id="links">
+      <ul>
+        <li>
+          <div className="iconWrapper">
+            <Facebook className="icon" />
+          </div>
+          <div>Like us</div>
+        </li>
+
+        <li>
+          <div className="iconWrapper">
+            <Twitter className="icon" />
+          </div>
+          <div>#flysas</div>
+        </li>
+
+        <li>
+          <div className="iconWrapper">
+            <Instagram className="icon" />
+          </div>
+          <div>Follow @SAS</div>
+        </li>
+
+        <li>
+          <div className="iconWrapper">
+            <LinkedIn className="icon" />
+          </div>
+          <div>Follow us</div>
+        </li>
+
+        <li>
+          <div className="iconWrapper">
+            <YouTube className="icon" />
+          </div>
+          <div>Subscribe to SAS</div>
+        </li>
+      </ul>
+    </div>
+  );
+};
