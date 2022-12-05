bcb31a96a876464592f20f0d18253e832201bac2
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-05 14:03:44 +0100

    navbar button active marker

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import { Search } from "@mui/icons-material";
 import { NavMapAbout } from "./NavMap/NavMapAbout"
 
 export const NavBar = () => {
+  const [about, setAbout] = useState(true);
+
+const handleAbout = () => {
+  setAbout(!about);
+}
+
   return (
     <div id="navbar">
       <div className="cell left"></div>
       <div className="cell center">
-        <button>About SAS</button>
+        <button onClick={(handleAbout)} className={about ? "active" : undefined}>About SAS</button>
         <button>Sustainability</button>
         <button>Investor relations</button>
         <button>Newsroom</button>
@@ -16,9 +23,7 @@ export const NavBar = () => {
       <div className="cell right">
         <Search className="searchIcon" />
       </div>
-      <div id="navMapBackground">
-        <NavMapAbout />
-      </div>
+        {about && <NavMapAbout />}
     </div>
   );
 };
--- src/components/NavMap/NavMapAbout.tsx
+++ src/components/NavMap/NavMapAbout.tsx
@@ -2,70 +2,90 @@ import { ArrowRight, Search } from "@mui/icons-material";
 
 export const NavMapAbout = () => {
   return (
-    <div className="navMapWrapper">
-      <h4><ArrowRight className="arrow"/> About SAS</h4>
-      <div className="navMap">
-        <ul>
-          <li><strong>This is SAS</strong></li>
-          <li>SAS Community support</li>
-          <li>SAS Code of Conduct</li>
-          <li>Financial goals</li>
-          <li><strong>Strategic priorities for a changing market</strong></li>
-          <li><strong>Business model</strong></li>
-        </ul>
-        <div className="divider"></div>
-        <ul>
-          <li><strong>Fleet</strong></li>
-          <li>Airbus A320neo/A321LR</li>
-          <li>Airbus A319/A320/A321</li>
-          <li>Airbus A330-300/A350-900</li>
-          <li>Boeing 737-700/800</li>
-          <li>E195LR</li>
-          <li>CRJ900</li>
-          <li>ATR-72-600</li>
-          <li>Aircraft on order</li>
-          <li>Alliances and partners – Star Alliance</li>
-          <li><strong>Board and management</strong></li>
-          <li>SAS Board of Directors</li>
-          <li>SAS Group Management</li>
-        </ul>
-        <div className="divider"></div>
-        <ul>
-          <li><strong>Corporate governance</strong></li>
-          <li>Legal structure</li>
-          <li>Reports</li>
-          <li>Shareholder meeting</li>
-          <li>Nomination committee</li>
-          <li>Board of Directors</li>
-          <li>Group Management</li>
-          <li>Remuneration principles</li>
-          <li>Auditors</li>
-          <li>Internal control</li>
-          <li>Financial information and reporting</li>
-          <li>Articles of Association</li>
-        </ul>
-        <div className="divider"></div>
-        <ul>
-          <li><strong>Bolagsstyrning</strong></li>
-          <li>Legal struktur</li>
-          <li>Rapporter</li>
-          <li>Bolagsstämma</li>
-          <li>Valberedning</li>
-          <li>Styrelse</li>
-          <li>Koncernledning</li>
-          <li>Ersättningsprinciper</li>
-          <li>Revisorer</li>
-          <li>Intern kontroll</li>
-          <li>Rapportering av finansiell information</li>
-          <li>Bolagsordning</li>
-        </ul>
-        <div className="divider"></div>
-        <ul>
-          <li><strong>SAS History</strong></li>
-          <li>History milestones</li>
-          <li>Presidents of SAS, beginning in 1946</li>
-          <li>The SAS Museum</li>
-        </ul>
+    <div id="navMapBackground">
+      <div className="navMapWrapper">
+        <h4>
+          <ArrowRight className="arrow" /> About SAS
+        </h4>
+        <div className="navMap">
+          <ul>
+            <li>
+              <strong>This is SAS</strong>
+            </li>
+            <li>SAS Community support</li>
+            <li>SAS Code of Conduct</li>
+            <li>Financial goals</li>
+            <li>
+              <strong>Strategic priorities for a changing market</strong>
+            </li>
+            <li>
+              <strong>Business model</strong>
+            </li>
+          </ul>
+          <div className="divider"></div>
+          <ul>
+            <li>
+              <strong>Fleet</strong>
+            </li>
+            <li>Airbus A320neo/A321LR</li>
+            <li>Airbus A319/A320/A321</li>
+            <li>Airbus A330-300/A350-900</li>
+            <li>Boeing 737-700/800</li>
+            <li>E195LR</li>
+            <li>CRJ900</li>
+            <li>ATR-72-600</li>
+            <li>Aircraft on order</li>
+            <li>Alliances and partners – Star Alliance</li>
+            <li>
+              <strong>Board and management</strong>
+            </li>
+            <li>SAS Board of Directors</li>
+            <li>SAS Group Management</li>
+          </ul>
+          <div className="divider"></div>
+          <ul>
+            <li>
+              <strong>Corporate governance</strong>
+            </li>
+            <li>Legal structure</li>
+            <li>Reports</li>
+            <li>Shareholder meeting</li>
+            <li>Nomination committee</li>
+            <li>Board of Directors</li>
+            <li>Group Management</li>
+            <li>Remuneration principles</li>
+            <li>Auditors</li>
+            <li>Internal control</li>
+            <li>Financial information and reporting</li>
+            <li>Articles of Association</li>
+          </ul>
+          <div className="divider"></div>
+          <ul>
+            <li>
+              <strong>Bolagsstyrning</strong>
+            </li>
+            <li>Legal struktur</li>
+            <li>Rapporter</li>
+            <li>Bolagsstämma</li>
+            <li>Valberedning</li>
+            <li>Styrelse</li>
+            <li>Koncernledning</li>
+            <li>Ersättningsprinciper</li>
+            <li>Revisorer</li>
+            <li>Intern kontroll</li>
+            <li>Rapportering av finansiell information</li>
+            <li>Bolagsordning</li>
+          </ul>
+          <div className="divider"></div>
+          <ul>
+            <li>
+              <strong>SAS History</strong>
+            </li>
+            <li>History milestones</li>
+            <li>Presidents of SAS, beginning in 1946</li>
+            <li>The SAS Museum</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
