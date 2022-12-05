1c05e6637a2cebb47074041490e6ba74416355d9
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-05 19:12:24 +0100

    featured cards

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { Cards } from './components/Cards';
 import { Carousel } from './components/Carousel';
 import { Header } from './components/Header';
-// import { NavBar } from './components/NavBar';
 import { Supplier } from './components/Supplier';
+import { Featured } from './components/Featured';
 
 function App() {
   return (
     <>
     <Header />
-    {/* <NavBar /> */}
     <Carousel />
     <Cards />
     <Supplier />
+    <Featured />
     </>
   );
 }
--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,3 +1,18 @@
 export const Featured = () => {
-    return <></>
-}
+  return (
+    <div id="featured">
+      <div className="feature">
+        <h3>Traffic Program</h3>
+      </div>
+      <div className="feature">
+        <h3>Annual & Sustainability Report FY2021</h3>
+      </div>
+      <div className="feature">
+        <h3>
+          Latest Share <br />
+          0.57 SEK
+        </h3>
+      </div>
+    </div>
+  );
+};
