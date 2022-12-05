62244c1364a6b0ade001f40eb8732399979c404d
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-06 00:01:45 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,33 @@
+import starAlliance from "./../images/star-alliance.png";
+
 export const Footer = () => {
-    return <>Footer</>
-}
+  return (
+    <div id="footer">
+      <section>
+        <p>
+          <strong>SAS 2020</strong>
+          <br />
+          SAS AB, registration number 556606-8499,
+          <br />
+          SE-195 87
+          <br />
+          Stockholm, Sweden
+        </p>
+      </section>
+      <section className="links">
+        <span>Book a trip with SAS</span>
+        <div className="divider"></div>
+        <span>Contacts</span>
+        <div className="divider"></div>
+        <span>SAS Cargo</span>
+        <div className="divider"></div>
+        <span>Usage of cookies</span>
+        <div className="divider"></div>
+        <span>Terms and conditions</span>
+      </section>
+      <section id="imageWrapper">
+        <img src={starAlliance} />
+      </section>
+    </div>
+  );
+};
