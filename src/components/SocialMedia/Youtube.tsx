71d77c438274c78e6584c566624d4ca09d581ee3
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-05 21:58:18 +0100

    youtube posts

--- src/components/SocialMedia/Youtube.tsx
+++ src/components/SocialMedia/Youtube.tsx
@@ -1,7 +1,23 @@
-export const Youtube = () => {
+import { YouTube as YoutubeIcon } from "@mui/icons-material";
+
+const Icon = () => {
     return (
-        <>
-        YouTube
-        </>
-        )
+        <div className="iconWrapper"><YoutubeIcon className="thumbIcon"/></div>
+    )
 }
+
+export const Youtube = () => {
+  return (
+    <div id="youtube" className="mediaWrapper">
+      <article>
+        <div className="image img1"><Icon /></div>
+      </article>
+      <article>
+        <div className="image img2"><Icon /></div>
+      </article>
+      <article>
+        <div className="image img3"><Icon /></div>
+      </article>
+    </div>
+  );
+};
