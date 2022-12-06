70174e3aaafbf62f00fe15b9142d11236ce0b6ca
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-06 18:18:20 +0100

    accordion section toggle

--- src/components/Navigation/NavAccordion.tsx
+++ src/components/Navigation/NavAccordion.tsx
@@ -1,24 +1,36 @@
+import { useState } from "react";
 import { navData } from "./navData";
 
 export const NavAccordion = () => {
+    const [sections, setSections] = useState<null | number>(null);
+    const [pages, setPages] = useState<null | number>(null);
+
+    const toggleSections = (titleIndex: number) => {
+        if (sections === titleIndex) {
+            setSections(null);
+        } else {
+            setSections(titleIndex);
+        }
+    }
+
   return (
     <div className="accordionContainer">
-      {navData.map((item, index) => (
-        <div className="itemBox" key={index}>
-          <div className="titleBox">
+      {navData.map((item, titleIndex) => (
+        <div className="itemBox" key={titleIndex}>
+          <div className="titleBox" onClick={() => toggleSections(titleIndex)}>
             <div className="title">{item.title}</div>
-            <span>+</span>
+            <span>{sections === titleIndex ? "-" : "+"}</span>
           </div>
           <div>
-            {item.sections.map((section, index) => (
-              <div key={index}>
+            {item.sections.map((section, sectionIndex) => (
+              <div key={sectionIndex} className={sections === titleIndex ? "" : "collapsed"}>
                 <div className="titleBox">
                 <div>{section.header}</div>
                 <span>+</span>
                 </div>
                 <div>
-                  {section.pages.map((page, index) => (
-                    <div className="pageBox" key={index}>
+                  {section.pages.map((page, pageIndex) => (
+                    <div className="pageBox collapsed" key={pageIndex}>
                       {page}
                     </div>
                   ))}
