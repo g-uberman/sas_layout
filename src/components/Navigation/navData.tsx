6b4624ffe960ae188fb1bef890899b7885117a31
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-06 19:24:44 +0100

    all nav data input

--- src/components/Navigation/navData.tsx
+++ src/components/Navigation/navData.tsx
@@ -1,9 +1,9 @@
 interface NavData {
-    title: string;
-    sections: {
-        header: string;
-        pages: string[];
-    }[];
+  title: string;
+  sections: {
+    header: string;
+    pages: string[];
+  }[];
 }
 
 export const navData: NavData[] = [
@@ -15,77 +15,335 @@ export const navData: NavData[] = [
         pages: [
           "SAS Community support",
           "SAS Code of Conduct",
-          "Financial goals"
-        ]
+          "Financial goals",
+        ],
       },
       {
         header: "Strategic priorities for a changing market",
-        pages: []
+        pages: [],
       },
       {
         header: "Business model",
-        pages: []
+        pages: [],
       },
       {
         header: "Fleet",
         pages: [
-            "Airbus A320neo/A321LR",
-            "Airbus A319/A320/A321",
-            "Airbus A330-300/A350-900",
-            "Boeing 737-700/800",
-            "E195LR",
-            "CRJ900",
-            "ATR-72-600",
-            "Aircraft on order",
-            "Alliances and partners – Star Alliance"
+          "Airbus A320neo/A321LR",
+          "Airbus A319/A320/A321",
+          "Airbus A330-300/A350-900",
+          "Boeing 737-700/800",
+          "E195LR",
+          "CRJ900",
+          "ATR-72-600",
+          "Aircraft on order",
+          "Alliances and partners – Star Alliance",
         ],
       },
       {
         header: "Board and management",
-        pages: [
-            "SAS Board of Directors",
-            "SAS Group Management"
-        ],
+        pages: ["SAS Board of Directors", "SAS Group Management"],
       },
       {
         header: "Corporate governance",
         pages: [
-            "Legal structure",
-            "Reports",
-            "Shareholder meeting",
-            "Nomination committee",
-            "Board of Directors",
-            "Group Management",
-            "Remuneration principles",
-            "Auditors",
-            "Internal control",
-            "Financial information and reporting",
-            "Articles of Association"
+          "Legal structure",
+          "Reports",
+          "Shareholder meeting",
+          "Nomination committee",
+          "Board of Directors",
+          "Group Management",
+          "Remuneration principles",
+          "Auditors",
+          "Internal control",
+          "Financial information and reporting",
+          "Articles of Association",
         ],
       },
       {
         header: "Bolagsstyrning",
         pages: [
-            "Legal struktur",
-            "Rapporter",
-            "Bolagsstämma",
-            "Valberedning",
-            "Styrelse",
-            "Koncernledning",
-            "Ersättningsprinciper",
-            "Revisorer",
-            "Intern kontroll",
-            "Rapportering av finansiell information",
-            "Bolagsordning"
+          "Legal struktur",
+          "Rapporter",
+          "Bolagsstämma",
+          "Valberedning",
+          "Styrelse",
+          "Koncernledning",
+          "Ersättningsprinciper",
+          "Revisorer",
+          "Intern kontroll",
+          "Rapportering av finansiell information",
+          "Bolagsordning",
         ],
       },
       {
         header: "SAS History",
         pages: [
-            "History milestones",
-            "Presidents of SAS, beginning in 1946",
-            "The SAS Museum"
+          "History milestones",
+          "Presidents of SAS, beginning in 1946",
+          "The SAS Museum",
         ],
+      },
+    ],
+  },
+
+  {
+    title: "Sustainability",
+    sections: [
+      {
+        header: "SAS view on sustainability",
+        pages: [
+          "Responsibility for sustainable development",
+          "Environmental Responsibility",
+          "Social Responsibility",
+          "Financial Responsibility",
+        ],
+      },
+      {
+        header: "SAS Stakeholders",
+        pages: [],
+      },
+      {
+        header: "Sustainability policies",
+        pages: [
+          "Sustainability policy",
+          "Environmental policy",
+          "Work environment policy",
+          "Diversity policy",
+          "Purchasing policy",
+          "Quality policy",
+        ],
+      },
+      {
+        header: "Sustainability related KPI's",
+        pages: ["Key Performance Indicators"],
+      },
+      {
+        header: "Flight emission calculator",
+        pages: [],
+      },
+      {
+        header: "Environmental Management System (ISO 14001)",
+        pages: [],
+      },
+      {
+        header: "Sustainability reports",
+        pages: [],
+      },
+      {
+        header: "Initiatives and activities",
+        pages: [
+          "Humanitarian support",
+          "Newer aircraft of the right size creates less emission",
+          "Renewable energy in the wings",
+          "SAS takes its social responsibility seriously",
+          "Work Environment at the airport",
+          "CO2 offset",
+          "State aid",
+        ],
+      },
+      {
+        header: "Sustainability contacts",
+        pages: [],
+      },
+    ],
+  },
+
+  {
+    title: "Investor relations",
+    sections: [
+      {
+        header: "Accelerating SAS forward",
+        pages: [],
+      },
+
+      {
+        header: "Shareholder program",
+        pages: [],
+      },
+
+      {
+        header: "Financial targets and achievements",
+        pages: [],
+      },
+
+      {
+        header: "Financial data",
+        pages: [
+          "Financial data",
+          "SAS operational traffic data",
+          "Financial definitions",
+        ],
+      },
+
+      {
+        header: "Debt and credit rating",
+        pages: [],
+      },
+      {
+        header: "The share",
+        pages: [
+          "Share capital development",
+          "Dividend and dividend policy",
+          "Shareholders",
+          "Insiders",
+          "Analyst coverage",
+        ],
+      },
+      {
+        header: "Recapitalization 2020",
+        pages: [],
+      },
+      {
+        header: "Financial reports",
+        pages: [
+          "Interim Reports",
+          "Annual Reports",
+          "Prospectus",
+          "SAS Fact Book",
+          "Other reports",
+        ],
+      },
+      {
+        header: "Press releases",
+        pages: [],
+      },
+      {
+        header: "SAS traffic figures",
+        pages: ["Traffic figures", "SAS traffic and capacity definitions"],
+      },
+      {
+        header: "Financial presentations",
+        pages: [],
+      },
+      {
+        header: "Calendar",
+        pages: ["Calendar"],
+      },
+      {
+        header: "Corporate governance",
+        pages: [],
+      },
+      {
+        header: "Financial information and reporting",
+        pages: [],
+      },
+      {
+        header: "Investor relations contacts",
+        pages: [],
+      },
+      {
+        header: "Subscribe and follow",
+        pages: [],
+      },
+    ],
+  },
+
+  {
+    title: "Newsroom",
+    sections: [
+      {
+        header: "Press releases",
+        pages: [
+          "Capital market",
+          "Traffic figures",
+          "SAS News",
+          "Star Alliance",
+        ],
+      },
+      {
+        header: "SAS Comments",
+        pages: [],
+      },
+      {
+        header: "Financial reports",
+        pages: [],
+      },
+      {
+        header: "Imagebank",
+        pages: [],
+      },
+      {
+        header: "Media contacts",
+        pages: [],
+      },
+      {
+        header: "Photo permission",
+        pages: [],
+      },
+      {
+        header: "Sponsorship requests",
+        pages: [],
+      },
+      {
+        header: "Subscribe and follow",
+        pages: [],
+      },
+    ],
+  },
+
+  {
+    title: "Career",
+    sections: [
+      {
+        header: "Working at SAS",
+        pages: [
+          "Benefits and compensations",
+          "Learning and development",
+          "Sustainability and innovation",
+        ],
+      },
+      {
+        header: "Teams",
+        pages: [
+          "Airport staff",
+          "Cabin crew",
+          "Corporate office",
+          "Engineering & Maintenance",
+          "IT & Digital",
+          "Pilot",
+        ],
+      },
+      {
+        header: "Vacancies",
+        pages: [],
+      },
+    ],
+  },
+
+  {
+    title: "Contact",
+    sections: [
+      {
+        header: "Customer contact",
+        pages: [],
+      },
+      {
+        header: "Media contacts",
+        pages: [],
+      },
+      {
+        header: "Investor relations contacts",
+        pages: [],
+      },
+      {
+        header: "Sustainability contacts",
+        pages: [],
+      },
+      {
+        header: "Supplier information",
+        pages: [
+          "SAS Supplier Code of Conduct",
+          "SAS Group General Terms and Conditions",
+        ],
+      },
+      {
+        header: "Usage of cookies",
+        pages: [],
+      },
+      {
+        header: "Terms and conditions for use of SAS website",
+        pages: [],
       },
     ],
   },
